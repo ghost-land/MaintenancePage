d75a97ceb31377fea5dd35a5b2b39b18f531a928
Firmin B <stereogaming18@gmail.com>
2022-07-27 13:41:07 +0200

    First

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app dark>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer padless>
+    <v-card
+      flat
+      tile
+      color="black"
+      class="lighten-1 white--text text-center footer"
+    >
+      <v-card-text>
+        <v-btn
+          class="mx-4 white--text"
+          href="https://discord.gg/a7vdfT6YPq"
+          icon
+        >
+          <svg style="width: 24px;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Discord</title><path fill="#FFF" d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z"/></svg>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://twitter.com/GhosteShop" icon>
+          <v-icon size="24px"> mdi-twitter </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://github.com/ghost-land" icon>
+          <v-icon size="24px"> mdi-github </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://raw.githubusercontent.com/ghost-land/ghost-land.github.io/main/ghosteshop.json" icon>
+          <v-icon size="24px"> mdi-code-json </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://ghosteshop.com/feed/news.xml" icon>
+          <v-icon size="24px"> mdi-rss </v-icon>
+        </v-btn>
+        <v-btn class="mx-4 white--text" href="https://wiki.ghosteshop.com/" icon>
+          <v-icon size="24px"> mdi-book-open-page-variant </v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Ghost eShop Maintenance',
+    }
+  },
+}
+</script>
+
+<style>
+.footer {
+  width: 100% !important;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <v-img src="favicon.ico" class="d-flex justify-center" style="padding:auto" max-width="128px" />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          We're sorry, but our server is in maintenance.
+        </v-card-title>
+        <v-card-text>
+          <p>
+            With the various attacks we have experienced, we have decided to reconfigure our systems to improve stability and resistance to these attacks. We will come back soon, sorry for the inconvenience.
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+          href="https://discord.gg/a7vdfT6YPq"
+        >
+          Join the discord
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
