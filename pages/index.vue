b1d7af4ca01b619bf9c82ad5c80ab23f2d783a80
Firmin B <stereogaming18@gmail.com>
2023-04-11 18:36:42 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,37 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <v-img src="favicon.ico" class="d-flex justify-center" style="padding:auto" max-width="128px" />
+        <v-img
+          src="favicon.ico"
+          class="d-flex justify-center"
+          style="padding: auto"
+          max-width="128px"
+        />
       </v-card>
       <v-card>
-        <v-card-title class="headline">
-          We're sorry, but our server is in maintenance.
-        </v-card-title>
+        <v-card-title class="headline"> </v-card-title>
         <v-card-text>
           <p>
-            We have chosen to put our server in maintenance. Learn more on our discord server.
+            Ghost eShop needs you to pay for the servers, because the sites and
+            especially the cdn and applications cost a certain amount every
+            month. The donations will be used mainly to pay the new servers and
+            the next projects such as the WiiU db and maybe a WiiU homebrew.
+            Here are the different costs:<br />
+            - Cost of the main server: 42$/month<br />
+            - Estimated cost of the new server : 80$/month<br />
+            - Maintenance cost (backup, domain name...) : 300$/year<br />This
+            makes a total cost of more than 1300$ per year. If we manage to
+            raise this 1300$, we will have enough money to continue the Ghost
+            eShop project for another year.<br /><b
+              >Thank you very much to all those who will make a donation as well
+              as all those who already support us.</b
+            >
           </p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-          href="https://discord.gg/a7vdfT6YPq"
-        >
-          Join the discord
-          </v-btn>
+          <v-btn href="https://ko-fi.com/ghostland"> Donate with Ko-Fi </v-btn>
+          <v-btn href="https://discord.gg/a7vdfT6YPq"> Join the discord </v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
