f347cf10a4c5e78cfb45b2c9748356e44b45e675
Ghost0159 <contact.ghost01@gmail.com>
2024-04-02 19:47:50 +0200

    Improve maintenance page

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,30 @@
         <v-card-title class="headline"> </v-card-title>
         <v-card-text>
           <p>
+            <br><b
+              >Ghost eShop is facing difficulties due to server costs. Please read this message.​</b><br />
             Ghost eShop needs you to pay for the servers, because the sites and
             especially the cdn and applications cost a certain amount every
-            month. The donations will be used mainly to pay the new servers and
-            the next projects such as the WiiU db and maybe a WiiU homebrew.
+            month. The donations will be used mainly to pay the servers and
+            the projects such as the WiiU DB, BETA 3DS.
             Here are the different costs:<br />
-            - Cost of the main server: 42$/month<br />
-            - Estimated cost of the new server : 80$/month<br />
-            - Maintenance cost (backup, domain name...) : 300$/year<br />This
-            makes a total cost of more than 1300$ per year. If we manage to
-            raise this 1300$, we will have enough money to continue the Ghost
-            eShop project for another year.<br /><b
+            - Cost of the main server: 200$/month<br />
+            - Estimated cost of the proxy : 100$/month<br />
+            - DDOS Protection + VM for Proxy cost : 200$/month<br />
+            - Maintenance cost (backup, domain name...) : 50$/month<br />
+            - Other cost : 20$/month<br />This
+            makes a total cost of more than 570$ per month (6840$/year). If we manage to
+            raise this 6840$, we will have enough money to continue the Ghost
+            eShop project for another year.<br />
+            <br>Ghost eShop will remain free for life, but unfortunately for members of
+            the Ghost Land Team and Ghost eShop Team, it has become difficult to afford
+            the monthly cost of over $570. It's important to note that the Ghost eShop Team
+            consists of a small team of 3 developers and 3 other members (such as graphic designers).
+            Therefore, we are requesting donations to keep Ghost eShop operational.
+            Once we receive the sum of $570, we will be able to cover the server costs,
+            and everything will function perfectly again.<br
+            >We sincerely apologize for any inconvenience and ask for your understanding.<br />
+            <br /><b
               >Thank you very much to all those who will make a donation as well
               as all those who already support us.</b
             >
